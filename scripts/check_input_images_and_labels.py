07e399351d3c5ee348f412b4e7cc452aff0709b7
Ishan Nangia <ishannangia.123@gmail.com>
2024-06-24 10:42:38 +0530

    Refactor name of project
    Why: Ideally, name of project should be the same as name of package inside
    it
    
    How: Changing the name from reefbuilder-segmentation to reefbuilder_segmentation
    
    Tags:

--- scripts/check_input_images_and_labels.py
+++ scripts/check_input_images_and_labels.py
@@ -1,8 +1,8 @@
 from reefbuilder_segmentation.checks.images import ImageChecker
 from reefbuilder_segmentation.checks.labels import LabelChecker
 
-source_image_folder = '/Users/ishannangia/github_repos/reefbuilder-segmentation/data/test_data/single_test_images'
-source_label_folder = '/Users/ishannangia/github_repos/reefbuilder-segmentation/rough/sample_coco_labels'
+source_image_folder = '/Users/ishannangia/github_repos/reefbuilder_segmentation/data/test_data/single_test_images'
+source_label_folder = '/Users/ishannangia/github_repos/reefbuilder_segmentation/rough/sample_coco_labels'
 
 if __name__ == "__main__":
     ic = ImageChecker(source_image_folder)
