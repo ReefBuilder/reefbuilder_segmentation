f171eab0096138fb75891c6e977074341989ada8
beatboxerish <ishannangia.123@gmail.com>
2024-07-16 09:46:08 +0530

    Add initial logging setup
    Why: Logging is needed to redirect output from stdout to a file
    so that we can easily search logs, errors, messages, etc.
    
    How: Using the inbuilt logging library
    
    Tags:

---
+++ reefbuilder_segmentation/__init__.py
@@ -0,0 +1,19 @@
+import logging.config
+import json
+
+logger = logging.getLogger("reefbuilder_segmentation")
+
+
+def setup_logging():
+    config_file = "/Users/ish/github_repos/reefbuilder_segmentation/reefbuilder_segmentation/logging_config.json"
+    with open(config_file) as f:
+        log_config = json.load(f)
+    logging.config.dictConfig(log_config)
+
+
+def main():
+    setup_logging()
+    logging.info("Logger has been setup. Logging now...")
+
+
+main()
