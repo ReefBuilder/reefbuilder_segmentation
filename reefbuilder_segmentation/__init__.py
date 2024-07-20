b76710e8a6566c4d941055afdda3d1255605ca12
beatboxerish <ishannangia.123@gmail.com>
2024-07-20 17:21:39 +0530

    Fix logging path
    Why: Full path was hardcoded as the logger filename in the config.
    This would have led to errors when running it anywhere else.
    
    How:
    
    Tags:

--- reefbuilder_segmentation/__init__.py
+++ reefbuilder_segmentation/__init__.py
@@ -1,6 +1,7 @@
 import logging.config
 import json
 import reefbuilder_segmentation.config as cfg
+import os
 
 logger = logging.getLogger(cfg.logger_name)
 
@@ -9,6 +10,10 @@ def setup_logging():
     config_file = cfg.log_file_path
     with open(config_file) as f:
         log_config = json.load(f)
+    log_file_path = log_config["handlers"]["file"]["filename"]
+    if not os.path.exists(log_file_path):
+        os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+    print(f"Log file can be found here: {log_file_path}")
     logging.config.dictConfig(log_config)
 
 
