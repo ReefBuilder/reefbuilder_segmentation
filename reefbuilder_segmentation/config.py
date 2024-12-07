b5ae463dfbd150538a0a87aede20e3fd7f8c5e78
beatboxerish <ishannangia.123@gmail.com>
2024-12-07 20:59:21 +0530

    Fix file path bug in config.py
    Why:
    
    How:
    
    Tags:

--- reefbuilder_segmentation/config.py
+++ reefbuilder_segmentation/config.py
@@ -1,7 +1,11 @@
+import os
+
 # file to store config or global variables
 
 accepted_image_formats = ["jpg", "jpeg", "png", "JPG", "JPEG", "PNG"]
 accepted_label_formats = ["json", "JSON"]
 base_yolo_model = "yolov8m-seg.pt"
 logger_name = "reefbuilder_segmentation"
-log_file_path = "/Users/ish/github_repos/reefbuilder_segmentation/reefbuilder_segmentation/logging_config.json"  # noqa
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+log_file_path = os.path.join(current_dir, "logging_config.json")
